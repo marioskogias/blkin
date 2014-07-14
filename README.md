511bdaac6be390dae45657b1dcd2f47462d33dd8
Marios Kogias <marioskogias@grnet.gr>
2014-07-14 11:32:17 +0300

    Add some definitions

---
+++ README.md
@@ -0,0 +1,12 @@
+#blkin
+
+blkin is a C/C++ library that enables you to get traces from C/C++ applications
+according to the tracing semantics mentioned in the Dapper paper[1] and
+implemented in Zipkin[2]
+
+As a tracing backend blkin uses LTTng
+
+## Dependencies
+
+* libboost-all-dev
+* lttng-tools
--- zipkin_trace.h
+++ zipkin_trace.h
@@ -22,15 +22,30 @@ TRACEPOINT_EVENT(
             char *, key, char *, val ),
         
         TP_FIELDS(
+                /*
+                 * Each span has a name mentioned on it in the UI
+                 * This is the trace name 
+                 */
                 ctf_string(trace_name, trace_name)
+                /*
+                 * Each trace takes place in a specific machine-endpoint
+                 * This is identified by a name, a port number and an ip
+                 */
                 ctf_string(service_name, service)
                 ctf_integer(int, port_no, port)
                 ctf_string(ip, ip)
+                /*
+                 * According to the tracing semantics each trace should have
+                 * a trace id, a span id and a parent span id 
+                 */
                 ctf_integer(long, trace_id, trace)
-        ctf_integer(long, span_id, span)
+                ctf_integer(long, span_id, span)
                 ctf_integer(long, parent_span_id, parent_span)
-        ctf_string(key, key)
-        ctf_string(val, val)
+                /*
+                 * The following is the real annotated information
+                 */
+                ctf_string(key, key)
+                ctf_string(val, val)
         ) 
 )
 TRACEPOINT_LOGLEVEL(
@@ -38,6 +53,9 @@ TRACEPOINT_LOGLEVEL(
         keyval, 
         TRACE_WARNING)
 
+/*
+ * In this event we follow the same semantics but we trace timestamp annotations
+ */
 
 TRACEPOINT_EVENT(
         zipkin,
@@ -53,7 +71,7 @@ TRACEPOINT_EVENT(
                 ctf_integer(int, port_no, port)
                 ctf_string(ip, ip)
                 ctf_integer(long, trace_id, trace)
-        ctf_integer(long, span_id, span)
+                ctf_integer(long, span_id, span)
                 ctf_integer(long, parent_span_id, parent_span)
                 ctf_string(event, event)
         ) 
